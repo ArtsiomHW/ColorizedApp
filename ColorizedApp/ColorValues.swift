ea05b030da1cd1232c7f872c08a09441d871ffb1
Artsiom <artsiom.hurynovich@gmail.com>
2024-10-27 21:35:24 +0100

    Logic and UI added

---
+++ ColorizedApp/ColorValues.swift
@@ -0,0 +1,16 @@
+//
+//  ColorValues.swift
+//  ColorizedApp
+//
+//  Created by Artem H on 27.10.24.
+//
+
+import Foundation
+
+struct ColorValues {
+    
+    var redValue: String = ""
+    var greenValue: String = ""
+    var blueValue: String = ""
+    
+}
--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol EditViewControllerDelegate: AnyObject {
+    func setViewColor(red: CGFloat, green: CGFloat, blue: CGFloat)
+}
+
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1)
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editVC = segue.destination as? EditViewController
+        editVC?.delegate = self
+        editVC?.uiColor = view.backgroundColor
+    }
 }
 
+// MARK: - EditViewControllerDelegate
+extension ViewController: EditViewControllerDelegate {
+    func setViewColor(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+}
